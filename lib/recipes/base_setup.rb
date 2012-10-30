2a99e43b23677348d5d2b4ce48329f9dfc15574d
Andreas Happe <andreashappe@snikt.net>
2012-10-31 00:25:21 +0100

    add: automatically install sudoers file
    This allows the configured user to do everything on the target
    system. Future releases should fix the behaviour by supplying
    user scripts within /usr/local/sbin and only granting access
    rights on those scripts

--- lib/recipes/base_setup.rb
+++ lib/recipes/base_setup.rb
@@ -1,28 +1,32 @@
-# TODO: run those commands as root (and not as normal user)
-
+# TODO: those commands should be run as root instead of the configured user
+# TODO: automatically activate support for /etc/sudoers.d/
 Capistrano::Configuration.instance(:must_exist).load do
   namespace :base_setup do
 
-	  task :prepare_root_stuff do
-		  logger.info "full deploy (as root) tasks"
-  	end
+    task :prepare_root_stuff do
+      # update package database
+      run "apt-get update"
+
+      copy_add_apt_repository
+      install_sudo
+      install_base_gem_dependencies
+    end
 
     task :copy_add_apt_repository do
       generate_from_template 'add-apt-repository', '/usr/local/bin/add-apt-repository'
       run "chmod +x /usr/local/bin/add-apt-repository"
     end
-    after "base_setup:prepare_root_stuff", "base_setup:copy_add_apt_repository"
 
     task :install_sudo do
-      run "apt-get update"
       run "apt-get -y install sudo"
-      # TODO/IDEA: add sudo rules for user
+
+      # for now give the defined user unlimited rights
+      # TODO: limit rights to some specially given commands in /usr/local/sbin/
+      generate_from_template 'root_rights_for_user', "/etc/sudoers.d/root_rights_for_#{user}"
     end
-    after "base_setup:prepare_root_stuff", "base_setup:install_sudo"
 
     task :install_base_gem_dependencies do
       run "apt-get -y install libmagickwand-dev libsqlite3-dev"
     end
-    after "base_setup:prepare_root_stuff", "base_setup:install_base_gem_dependencies"
   end
 end
